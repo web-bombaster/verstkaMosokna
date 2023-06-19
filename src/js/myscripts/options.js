d1fd2c83c150d0fab4504d3fcb2fa59c7e5cb81e
web-bombaster <skorpion_05@inbox.ru>
2023-06-19 23:55:08 +0300

    js переименование переменных

--- src/js/myscripts/options.js
+++ src/js/myscripts/options.js
@@ -17,14 +17,14 @@ optionsToggle();
 // раздаем классы toggle первым элементам внутри родительских, предварительно почистив их у других элементов
 let initClassToggleItem = function(parentElement, childElement) {
     if (document.querySelector(parentElement)) {
-        const variantsWrapper = document.querySelectorAll(parentElement);
-        variantsWrapper.forEach(function(element, index) {
-            let ttt = variantsWrapper[index].querySelectorAll(childElement);
-            ttt.forEach(function(element, index) {
-                ttt[index].classList.remove('toggle');
+        const parents = document.querySelectorAll(parentElement);
+        parents.forEach(function(element, index) {
+            let childrens = parents[index].querySelectorAll(childElement);
+            childrens.forEach(function(element, index) {
+                childrens[index].classList.remove('toggle');
             });
 
-            variantsWrapper[index].querySelector(childElement).classList.add('toggle');
+            parents[index].querySelector(childElement).classList.add('toggle');
         });
     };
 };
